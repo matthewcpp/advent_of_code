3d9916c6deec9d57d3f52c95d1adaf6f77272142
Matthew LaRocca <mlarocca.cpp@gmail.com>
2022-12-04 04:42:48 -0800

    2022 Day 4

---
+++ 2022/4-1.cpp
@@ -0,0 +1,38 @@
+
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <sstream>
+
+bool contains(std::pair<uint32_t, uint32_t> const & a, std::pair<uint32_t, uint32_t> const & b);
+
+int main (int argc, char** argv) {
+    //auto const input_file_path = std::string(INPUT_DIRECTORY) + "4.test.txt";
+    auto const input_file_path = std::string(INPUT_DIRECTORY) + "4.txt";
+
+    std::ifstream input_file(input_file_path);
+
+    std::string current_line;
+    char ch;
+    uint32_t fully_contained = 0;
+
+    while(std::getline(input_file, current_line)) {
+        std::pair<uint32_t, uint32_t> a, b;
+        std::istringstream str_stream(current_line);
+        str_stream >> a.first >> ch >> a.second >> ch >> b.first >> ch >> b.second;
+
+        if (contains(a,b) || contains(b, a)) {
+            fully_contained += 1;
+        }
+    }
+
+    std::cout << fully_contained << std::endl;
+
+    return 0;
+}
+
+bool contains(std::pair<uint32_t, uint32_t> const & a, std::pair<uint32_t, uint32_t> const & b) {
+    return a.first >= b.first && a.second <=b.second;
+}
---
+++ 2022/4-2.cpp
@@ -0,0 +1,44 @@
+
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <sstream>
+
+bool overlaps(std::pair<uint32_t, uint32_t> const & a, std::pair<uint32_t, uint32_t> const & b);
+
+int main (int argc, char** argv) {
+    //auto const input_file_path = std::string(INPUT_DIRECTORY) + "4.test.txt";
+    auto const input_file_path = std::string(INPUT_DIRECTORY) + "4.txt";
+
+    std::ifstream input_file(input_file_path);
+
+    std::string current_line;
+    char ch;
+    uint32_t overlapping = 0;
+
+    while(std::getline(input_file, current_line)) {
+        std::pair<uint32_t, uint32_t> a, b;
+        std::istringstream str_stream(current_line);
+        str_stream >> a.first >> ch >> a.second >> ch >> b.first >> ch >> b.second;
+
+        if (overlaps(a,b)) {
+            overlapping += 1;
+        }
+    }
+
+    std::cout << overlapping << std::endl;
+
+    return 0;
+}
+
+bool overlaps(std::pair<uint32_t, uint32_t> const & a, std::pair<uint32_t, uint32_t> const & b) {
+    if (a.first > b.second)
+        return false;
+    
+    if (a.second < b.first)
+        return false;
+
+    return true;
+}
