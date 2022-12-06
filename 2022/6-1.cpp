bb2f3b04a9623708e4607c6dae20ef85a4bbd7e4
Matthew LaRocca <mlarocca.cpp@gmail.com>
2022-12-06 05:00:25 -0800

    2022 Day 6

---
+++ 2022/6-1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv) {
+    //auto const input_file_path = std::string(INPUT_DIRECTORY) + "6.test.txt";
+    auto const input_file_path = std::string(INPUT_DIRECTORY) + "6.txt";
+
+    std::ifstream input_file(input_file_path);
+    std::string input_str;
+    while (std::getline(input_file, input_str)) {
+        int start = 0, end = 1;
+
+        while (end < input_str.length()) {
+            for (int i = end - 1; i >= start; i--) {
+                if (input_str[i] == input_str[end]) {
+                    start = i + 1;
+                    break;
+                }
+            }
+
+            if (end - start == 3){
+                std::cout << end + 1 << '(' << input_str.substr(start, 4) << ')' << std::endl;
+                break;
+            }
+
+            end += 1;
+        }
+    }
+
+    return 0;
+}
+
+
