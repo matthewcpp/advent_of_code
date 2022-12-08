764162f207cc36859a4e691669b4dd8b26248b49
Matthew LaRocca <mlarocca.cpp@gmail.com>
2022-12-08 05:08:52 -0800

    2022 Day 8

---
+++ 2022/8-1.cpp
@@ -0,0 +1,59 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#define HASH(y,x) (((y) << 32) | (x & 0xFFFFFFFF))
+
+int main(int argc, char** argv) {
+    //auto const input_file_path = std::string(INPUT_DIRECTORY) + "8.test.txt";
+    auto const input_file_path = std::string(INPUT_DIRECTORY) + "8.txt";
+
+    std::vector<std::string> grid;
+    std::string current_line;
+    std::ifstream input_file(input_file_path);
+    std::unordered_set<size_t> visible_spaces;
+
+    while (std::getline(input_file, current_line))
+        grid.push_back(current_line);
+    
+    for (size_t y = 0; y < grid.size(); y++) {
+        char left_height = 0, right_height = 0;
+
+        for (size_t x = 0; x < grid[y].size(); x++) {
+            if (grid[y][x] > left_height) {
+                visible_spaces.insert(HASH(y, x));
+                left_height = grid[y][x];
+            }
+
+            size_t right_index = grid[y].size() - 1 - x;
+            if (grid[y][right_index] > right_height) {
+                visible_spaces.insert(HASH(y, right_index));
+                right_height = grid[y][right_index];
+            }
+        }
+    }
+
+    for (size_t x = 0; x < grid[0].size(); x++) {
+        char top_height = 0, bottom_height = 0;
+
+        for (size_t y = 0; y < grid.size(); y++) {
+            if (grid[y][x] > top_height) {
+                visible_spaces.insert(HASH(y, x));
+                top_height = grid[y][x];
+            }
+
+            size_t bottom_index = grid.size() - 1 - y;
+            if (grid[bottom_index][x] > bottom_height) {
+                visible_spaces.insert(HASH(bottom_index, x));
+                bottom_height = grid[bottom_index][x];
+            }
+        }
+    }
+
+    std::cout << visible_spaces.size() << std::endl;
+
+    return 0;
+}
---
+++ 2022/8-2.cpp
@@ -0,0 +1,67 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+uint32_t calc_senic_score(std::vector<std::vector<uint8_t>> const & grid, int x, int y);
+
+int main(int argc, char** argv) {
+    //auto const input_file_path = std::string(INPUT_DIRECTORY) + "8.test.txt";
+    auto const input_file_path = std::string(INPUT_DIRECTORY) + "8.txt";
+
+    std::vector<std::vector<uint8_t>> grid;
+    std::string current_line;
+    std::ifstream input_file(input_file_path);
+
+    while (std::getline(input_file, current_line)) {
+        grid.emplace_back();
+        auto & row = grid.back();
+
+        for (auto ascii : current_line) {
+            row.push_back(ascii - '0');
+        }
+    }
+    uint32_t max_secnic_score = 0;
+
+    for (int y = 0; y < grid.size(); y++) {
+        for (int x = 0; x < grid[y].size(); x ++) {
+            max_secnic_score = std::max(max_secnic_score, calc_senic_score(grid, x, y));
+        }
+    }
+
+    std::cout << max_secnic_score << std::endl;
+
+    return 0;
+}
+
+uint32_t calc_senic_score(std::vector<std::vector<uint8_t>> const & grid, int x, int y) {
+    uint32_t left_score = 0, right_score = 0, top_score = 0, bottom_score = 0;
+
+    for (int left = x - 1; left >= 0; left--) {
+        left_score += 1;
+        if (grid[y][left] >= grid[y][x])
+            break;
+    }
+
+    for (int right = x + 1; right < grid[y].size(); right++) {
+        right_score += 1;
+        if (grid[y][right] >= grid[y][x])
+            break;
+    }
+
+    for (int top = y - 1; top >= 0; top--) {
+        top_score += 1;
+        if (grid[top][x] >= grid[y][x])
+            break;
+    }
+
+    for (int bottom = y + 1; bottom < grid.size(); bottom++) {
+        bottom_score += 1;
+        if (grid[bottom][x] >= grid[y][x])
+            break;
+    }
+
+    return left_score * right_score * top_score * bottom_score;
+}
